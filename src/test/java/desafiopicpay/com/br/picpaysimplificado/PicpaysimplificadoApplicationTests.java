246688d517079308ca23c15b600b3129addb255a
LucasMatricarde <lucasmatricarde@hotmail.com>
2023-11-04 11:51:56 -0300

    first commit

---
+++ src/test/java/desafiopicpay/com/br/picpaysimplificado/PicpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package desafiopicpay.com.br.picpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
